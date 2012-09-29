b66f243787ae665bf4caa296ef5d2e7c0b9d90d2
Christian Urich <christian.urich@gmail.com>
2012-09-29 14:31:35 +0200

    works

--- src/placehouseholds.cpp
+++ src/placehouseholds.cpp
@@ -1,3 +1,29 @@
+/**
+ * @file
+ * @author  Chrisitan Urich <christian.urich@gmail.com>
+ * @version 1.0
+ * @section LICENSE
+ *
+ * This file is part of DynaMind
+ *
+ * Copyright (C) 2012  Christian Urich
+
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ */
+
 #include "placehouseholds.h"
 
 DM_DECLARE_NODE_NAME(PlaceHouseholds, PowerVIBe)
@@ -18,33 +44,51 @@ int PlaceHouseholds::chooseHousehold(int (&hh)[5])
             vChoose.push_back(i);
     }
 
+    if (vChoose.size() == 0)
+        return -1;
+
     int chooser= rand() % vChoose.size();
 
+    return vChoose[chooser];
+}
+
+int PlaceHouseholds::chooseBuilding(std::vector<int> &buildings)
+{
+    std::vector<int> vChoose;
+    for (unsigned int i = 0; i < buildings.size(); i++){
+        if (buildings[i] > 0)
+            vChoose.push_back(i);
+    }
+    if (vChoose.size() == 0)
+        return -1;
+    int chooser= rand() % vChoose.size();
     return vChoose[chooser];
 }
 
 PlaceHouseholds::PlaceHouseholds()
 {
     grids = DM::View("GRID", DM::FACE, DM::READ);
-    grids.getAttribute("HH01");
-    grids.getAttribute("HH02");
-    grids.getAttribute("HH03");
-    grids.getAttribute("HH04");
-    grids.getAttribute("HH05");
+    grids.getAttribute("hh01");
+    grids.getAttribute("hh02");
+    grids.getAttribute("hh03");
+    grids.getAttribute("hh04");
+    grids.getAttribute("hh05");
     grids.getAttribute("BUILDING");
 
     buildings = DM::View("BUILDING",  DM::FACE, DM::READ);
     buildings.getAttribute("residential_units");
 
 
+
     households = DM::View("HOUSEHOLD",  DM::COMPONENT ,DM::WRITE);
     households.addAttribute("id");
     households.addLinks("BUILDING", buildings);
+    buildings.addLinks("HOUSEHOLD", households);
 
-    persons = DM::View("Persons", DM::COMPONENT ,DM::WRITE);
+    persons = DM::View("PERSON", DM::COMPONENT ,DM::WRITE);
     persons.addAttribute("id");
     persons.addLinks("HOUSEHOLD", households);
-
+    households.addLinks("PERSON", persons);
 
     std::vector<DM::View> dataset;
     dataset.push_back(grids);
@@ -62,40 +106,83 @@ void PlaceHouseholds::run()
     std::vector<std::string> gridUuids = city->getUUIDsOfComponentsInView(grids);    
 
 
+    int household_id = 0;
+    int person_id = 0;
     foreach (std::string gUuid, gridUuids) {
           DM::Component * grid = city->getComponent(gUuid);
 
           int hh[5];
-          hh[0] = (int) grid->getAttribute("HH01")->getDouble();
-          hh[1] = (int) grid->getAttribute("HH02")->getDouble();
-          hh[2] = (int) grid->getAttribute("HH03")->getDouble();
-          hh[3] = (int) grid->getAttribute("HH04")->getDouble();
-          hh[4] = (int) grid->getAttribute("HH05")->getDouble();
+          hh[0] = (int) grid->getAttribute("hh01")->getDouble();
+          hh[1] = (int) grid->getAttribute("hh02")->getDouble();
+          hh[2] = (int) grid->getAttribute("hh03")->getDouble();
+          hh[3] = (int) grid->getAttribute("hh04")->getDouble();
+          hh[4] = (int) grid->getAttribute("hh05")->getDouble();
 
           //Get Buildings
           Attribute  * attrBuildings = grid->getAttribute("BUILDING");
           std::vector<LinkAttribute> lBuildings = attrBuildings->getLinks();
-          int numberOfBuildings = lBuildings.size();
-
           std::vector<int> avalibleUnits;
+          std::vector<std::string> buildingUUIDs;
           foreach (LinkAttribute lB, lBuildings) {
-              //int avalible units
-              //avalibleUnits
+              DM::Component * b = city->getComponent(lB.uuid);
+              int runits = (int) b->getAttribute("residential_units")->getDouble();
+              if (runits>0) {
+                avalibleUnits.push_back(runits);
+                buildingUUIDs.push_back(lB.uuid);
+              }
           }
+
 
           while (this->sumHouseholds(hh) > 0) {
-              //int chooser = this->chooseHousehold(hh);
+              household_id++;
+              int chooser = this->chooseHousehold(hh);
+              int chooser_building = this->chooseBuilding(avalibleUnits);
 
+              if (chooser == -1 || chooser_building == -1)
+                  break;
+              //Create Household
+              Component * h = new Component();
+              h->addAttribute("id", household_id);
 
+              //Link Household - Building
+              Attribute linkB("BUILDING");
+              linkB.setLink(buildings.getName(), buildingUUIDs[chooser_building]);
+              h->addAttribute(linkB);
 
+              //Link Building Household
+              Component * building = city->getComponent( buildingUUIDs[chooser_building]);
+              Attribute * bh = building->getAttribute("HOUSEHOLD");
+              bh->setLink(households.getName(), h->getUUID());
 
+              city->addComponent(h, households);
 
+              //Create Persons
+              for(int i = 0; i < chooser+1; i++) {
+                  person_id++;
+                  Component * p = new Component();
+                  p->addAttribute("id", person_id);
 
+                  //Link Person - Houshold
+                  Attribute l("HOUSEHOLD");
+                  l.setLink(households.getName(), h->getUUID());
+                  p->addAttribute(l);
+
+                  //Link Household - Person
+                  Attribute * hp = h->getAttribute("PERSON");
+                  hp->setLink(persons.getName(), p->getUUID());
+
+                  city->addComponent(p, persons);
+              }
+
+              //Remove Reidential Unit
+              avalibleUnits[chooser_building] = avalibleUnits[chooser_building]-1;
+
+              //Remove Household
+              hh[chooser] = hh[chooser]-1;
           }
 
-
     }
-
-
+    Logger(Debug) << "Created Households " << household_id;
+    Logger(Debug) << "Created Persons " << person_id;
 }
 
