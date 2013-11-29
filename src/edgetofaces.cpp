85abe4df156f42549b25cbb32844584a95c34616
Christian Urich <christian.urich@gmail.com>
2013-11-30 09:13:34 +1100

    added tol

--- src/edgetofaces.cpp
+++ src/edgetofaces.cpp
@@ -40,6 +40,7 @@ void EdgeToFaces::init()
 
 	edgeLayer = DM::View(edgeLayerName, DM::EDGE, DM::READ);
 	newFace = DM::View(newFaceName, DM::FACE, DM::WRITE);
+
 	std::vector<DM::View> datastream;
 	datastream.push_back(edgeLayer);
 	datastream.push_back(newFace);
@@ -53,8 +54,12 @@ EdgeToFaces::EdgeToFaces()
 {
 	edgeLayerName = "";
 	newFaceName = "";
+	tolerance = 0.01;
+	snapping = false;
 	this->addParameter("edgeLayerName", DM::STRING, &edgeLayerName);
 	this->addParameter("newFaceName", DM::STRING, &newFaceName);
+	this->addParameter("tolerance", DM::DOUBLE, &tolerance);
+	this->addParameter("snapping", DM::BOOL, &snapping);
 }
 
 
@@ -62,7 +67,7 @@ EdgeToFaces::EdgeToFaces()
 void EdgeToFaces::run()
 {
 	DM::System * sys = this->getData("Data");
-	DM::System s = CGALGeometry::ShapeFinder(sys,edgeLayer, newFace);
+	DM::System s = CGALGeometry::ShapeFinder(sys,edgeLayer, newFace, this->snapping, this->tolerance);
 
 	std::vector<std::string> uuids = s.getUUIDs(newFace);
 
@@ -72,7 +77,7 @@ void EdgeToFaces::run()
 		DM::Face * f = s.getFace(uuid);
 		std::vector<DM::Node*> nl_old = TBVectorData::getNodeListFromFace(&s, f);
 		std::vector<DM::Node*> nl;
-		for  (unsigned int i = 0; i < nl_old.size()-1; i++) {
+		for  (unsigned int i = 0; i < nl_old.size(); i++) {
 			if (spnh.findNode(nl_old[i]->getX(), nl_old[i]->getY(), 0.001)) {
 				Logger(Debug) << "Node exists already, not added";
 				continue;
@@ -80,7 +85,6 @@ void EdgeToFaces::run()
 			DM::Node * n = spnh.addNode(nl_old[i]->getX(), nl_old[i]->getY(),nl_old[i]->getZ(), 0.001);
 			nl.push_back(n);
 		}
-		nl.push_back(nl[0]);
 		sys->addFace(nl, newFace);
 	}
 
